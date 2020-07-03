7a1505a3fe130517ccbc647760894c48107e7643
officialcybersaint <darkocheykov@gmail.com>
2020-07-03 03:33:22 -0400

    Implementation of webcrawler
    using BeautifulSoup and request libraries to grab search results from google queries and parse, and request pages to save into HTML files within the dataset directory and its query topic subdirectories.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # categorycrawler
 A web crawler application meant for scraping the web and categorizing webpages via content and keywords
+
+Ran using a script that uses BeautifulSoup to parse google queries and obtain their constituent result links, later requesting each link and writes it to it's own HTML file.
+
+Automatically creates a ./dataset/ folder to contain all HTML files, and will also create sub-directories to organize each page by topic (query).
---
+++ webcrawler.py
@@ -0,0 +1,71 @@
+import urllib.request
+import requests
+from os import path, mkdir
+from shutil import rmtree
+from bs4 import BeautifulSoup
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
+headers = {"user-agent" : USER_AGENT}
+
+ILL_CHARS = ['<', '>', ':', '"', '\\', '|', '?', '*']
+
+count_q = int(input("how many queries? "))
+queries = []
+for x in range(count_q):
+    i = input(f"query {x+1}: ")
+    queries.append(i)
+print(queries)
+
+if path.exists("./dataset/"):
+    rmtree("./dataset/")
+mkdir("./dataset/")
+
+
+for x in range(count_q):
+    print(f"query {x+1}")
+    query = queries[x]
+    mkdir(f'./dataset/{query}')
+    query.replace(' ', "+")
+    URL  = f"https://google.com/search?q={query}"
+
+    
+    resp = requests.get(URL, headers=headers)
+
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
+
+    #print(soup)
+    results = []
+    for g in soup.find_all('div', class_='r'):
+        anchors = g.find_all('a')
+        if anchors:
+            link = anchors[0]['href']
+            title = g.find('h3').text
+            item = {
+                "title": title,
+                "link": link
+            }
+            results.append(item)
+
+    for x in results:
+        print(f"{x}")
+        url = x.get('link')
+        response = requests.get(url, headers=headers)
+        if resp.status_code != 200:
+            continue
+
+        filen = f"./dataset/{query}/{x.get('title')}.html"
+        for ch in ILL_CHARS:
+            if ch in filen:
+                filen = filen.replace(ch, '')
+        print(filen)
+        #urllib.request.urlretrieve(url, filename)
+        
+        with open(filen, 'wb') as f:
+            f.write(response.content)
+
+        # anchor = scrapeAnchor(x.get('link'), headers)
+        # filename = f"./dataset/{query}/{x.get('title')}.html"
+        # with open(filename, 'wb') as f:
+        #     f.write(anchor)
+    
