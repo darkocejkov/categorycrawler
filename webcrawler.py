e75fccb396a13a989f0d0bf4a59868ac7fdcbbd3
officialcybersaint <darkocheykov@gmail.com>
2020-07-24 17:45:57 -0400

    Code commenting
    commented major code

--- webcrawler.py
+++ webcrawler.py
@@ -4,39 +4,37 @@ from os import path, mkdir
 from shutil import rmtree
 from bs4 import BeautifulSoup
 
+#define user agent for query to force desktop viewing for optimal scraping
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0"
 headers = {"user-agent" : USER_AGENT}
 
-ILL_CHARS = ['<', '>', ':', '"', '\\', '|', '?', '*']
+ILL_CHARS = ['<', '>', ':', '"', '\\', '|', '?', '*'] #array of illegal characters in order to avoid windows filename errors
 
-count_q = int(input("how many queries? "))
-count_r = int(input("how many results? "))
+count_q = int(input("how many queries? ")) #ask about how many queries (topics) to gather
 queries = []
-for x in range(count_q):
-    i = input(f"query {x+1}: ")
+for x in range(count_q): #loop and input query names appended into the query array
+    i = input(f"query {x+1}: ") 
     queries.append(i)
 print(queries)
 
-if path.exists("./dataset/"):
+if path.exists("./dataset/"): #create (or remove first then create) directory to hold dataset
     rmtree("./dataset/")
 mkdir("./dataset/")
 
 
-for x in range(count_q):
-    c = 0
-    print(f"query {x+1}")
+for x in range(count_q): #BEGIN query loop
+    print(f"query {x+1}") 
     query = queries[x]
-    mkdir(f'./dataset/{query}')
-    query.replace(' ', "+")
-    URL  = f"https://google.com/search?q={query}"
+    mkdir(f'./dataset/{query}') #make the category directory
+    query.replace(' ', "+") #replace spaces with + so that it works in the Google query search
+    URL  = f"https://google.com/search?q={query}" 
 
     
-    resp = requests.get(URL, headers=headers)
+    resp = requests.get(URL, headers=headers) #use requests library to request the Google search results webpage
 
-    if resp.status_code == 200:
-        soup = BeautifulSoup(resp.content, "html.parser")
+    if resp.status_code == 200: #code 200 means successful request
+        soup = BeautifulSoup(resp.content, "html.parser") 
 
-    #print(soup)
     results = []
     for g in soup.find_all('div', class_='r'):
         anchors = g.find_all('a')
